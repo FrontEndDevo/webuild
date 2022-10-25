4a21bba692b41e646bea262b3df7213af7901329
FrontEndDevo <mylinkacco@gmail.com>
2022-10-25 10:48:16 +0200

    Add google font (Roboto)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="bg-red-500">Hello world</h1>;
+      <h1 className="bg-red-500">Hello world</h1>
     </div>
   );
 }
