8663812c7a38ac6e05ab6e526244fc446b9c5db9
FrontEndDevo <mylinkacco@gmail.com>
2022-10-25 11:34:31 +0200

    Add fontawesome library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolcano } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div>
       <h1 className="bg-red-500">Hello world</h1>
+      <FontAwesomeIcon icon={faVolcano} className="text-orange-600" />
+      <FontAwesomeIcon icon={faBuilding} />
     </div>
   );
 }
