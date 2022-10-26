3df3ead8b8e5ecb4028c1ed9ffffb37f6cde581c
FrontEndDevo <mylinkacco@gmail.com>
2022-10-26 13:40:39 +0200

    Initiate Service component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Navbar from "./components/NavigationBar/Navbar";
+import Service from "./components/Service/Service";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
       </Routes>
     </div>
   );
