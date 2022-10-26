39a7316f241f5017aad4506c0c32027fc6ae2fb2
FrontEndDevo <mylinkacco@gmail.com>
2022-10-26 14:20:30 +0200

    Edit images & implement basic form of service & import all icons and images

---
+++ src/components/Service/Importer.js
@@ -0,0 +1,15 @@
+import firstService from "../../assets/images/firstService.jpg";
+import secondService from "../../assets/images/secondService.jpg";
+import thirdService from "../../assets/images/thirdService.jpg";
+import fourthService from "../../assets/images/fourthService.jpg";
+import fifthService from "../../assets/images/fifthService.jpg";
+import sixthService from "../../assets/images/sixthService.jpg";
+
+export {
+  firstService,
+  secondService,
+  thirdService,
+  fourthService,
+  fifthService,
+  sixthService,
+};
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import {
+  firstService,
+  secondService,
+  thirdService,
+  fourthService,
+  fifthService,
+  sixthService,
+} from "./Importer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuildingUser,
+  faHouseChimney,
+  faSitemap,
+  faScrewdriverWrench,
+  faPalette,
+  faPaintBrush,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <div>
+      <h2>
+        we provide <span>the best</span>construction services
+      </h2>
+      <div className="services">
+        <div className="service">
+          <img src={firstService} alt="" />
+          <div className="info">
+            <FontAwesomeIcon icon={faBuildingUser} />
+            <h4>Building consttuction</h4>
+            <p>
+              Duo dolore et diam sed ipsum stet amet duo diam. Rebum amet ut
+              amet sed erat sed sed amet magna elitr amet kasd diam duo
+            </p>
+            <button>
+              Read More <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Service
+export default Service;
