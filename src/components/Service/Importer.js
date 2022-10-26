8478c06411f627725221c931bbd6a21910ce5efa
FrontEndDevo <mylinkacco@gmail.com>
2022-10-26 14:47:11 +0200

    Move data arrays to Importer function to keep Service component more lean

--- src/components/Service/Importer.js
+++ src/components/Service/Importer.js
@@ -1,3 +1,4 @@
+// Importing (Images) & Arrange them:
 import firstService from "../../assets/images/firstService.jpg";
 import secondService from "../../assets/images/secondService.jpg";
 import thirdService from "../../assets/images/thirdService.jpg";
@@ -5,11 +6,58 @@ import fourthService from "../../assets/images/fourthService.jpg";
 import fifthService from "../../assets/images/fifthService.jpg";
 import sixthService from "../../assets/images/sixthService.jpg";
 
-export {
+// Import (icons) from FontAwesome & arrange them in an array:
+import {
+  faBuildingUser,
+  faHouseChimney,
+  faSitemap,
+  faScrewdriverWrench,
+  faPalette,
+  faPaintBrush,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Arrange (images) in an array.
+const serviceImages = [
   firstService,
   secondService,
   thirdService,
   fourthService,
   fifthService,
   sixthService,
-};
+];
+
+// Arrange (icons) in an array.
+const serviceIcons = [
+  faBuildingUser,
+  faHouseChimney,
+  faSitemap,
+  faScrewdriverWrench,
+  faPalette,
+  faPaintBrush,
+];
+
+export { serviceImages, serviceIcons };
+
+// Create (Titles) and (Descriptions) arrays & export them:
+
+// Prepare (titles) and store them in an array.
+const serviceTitles = [
+  "Building Constructions",
+  "House Renovation",
+  "Architecture Design",
+  "Interior Design",
+  "Fixing & Support",
+  "Painting",
+];
+
+// Prepare (descriptions) and store them in an array.
+const serviceDescriptions = [
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  "Numquam eius necessitatibus repudiandae nostrum odit iste sed expedita nisi iure,",
+  "rem culpa dolore architecto totam, exercitationem natus vel",
+  "eveniet autem quisquam laborum beatae quam inventore!",
+  "Dolorem repellendus facere nobis debitis non. Officia error minima voluptas",
+  "reiciendis asperiores consequuntur rem unde ipsum ad, libero officiis temporibus accusantium,",
+];
+
+export { serviceTitles, serviceDescriptions };
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,64 +1,13 @@
-// Import images from Importer helper.
+import SingleService from "./SingleService/SingleService";
+// Import (Data Arrays) from Importer helper.
 import {
-  firstService,
-  secondService,
-  thirdService,
-  fourthService,
-  fifthService,
-  sixthService,
+  serviceImages,
+  serviceIcons,
+  serviceTitles,
+  serviceDescriptions,
 } from "./Importer";
 
-// Import icons from Importer helper.
-import {
-  faBuildingUser,
-  faHouseChimney,
-  faSitemap,
-  faScrewdriverWrench,
-  faPalette,
-  faPaintBrush,
-} from "@fortawesome/free-solid-svg-icons";
-import SingleService from "./SingleService/SingleService";
-
 const Service = () => {
-  // Arrange (images) in an array.
-  const serviceImages = [
-    firstService,
-    secondService,
-    thirdService,
-    fourthService,
-    fifthService,
-    sixthService,
-  ];
-  // Arrange (icons) in an array.
-  const serviceIcons = [
-    faBuildingUser,
-    faHouseChimney,
-    faSitemap,
-    faScrewdriverWrench,
-    faPalette,
-    faPaintBrush,
-  ];
-
-  // Prepare (titles) and store them in an array.
-  const serviceTitles = [
-    "Building Constructions",
-    "House Renovation",
-    "Architecture Design",
-    "Interior Design",
-    "Fixing & Support",
-    "Painting",
-  ];
-
-  // Prepare (descriptions) and store them in an array.
-  const serviceDescriptions = [
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-    "Numquam eius necessitatibus repudiandae nostrum odit iste sed expedita nisi iure,",
-    "rem culpa dolore architecto totam, exercitationem natus vel",
-    "eveniet autem quisquam laborum beatae quam inventore!",
-    "Dolorem repellendus facere nobis debitis non. Officia error minima voluptas",
-    "reiciendis asperiores consequuntur rem unde ipsum ad, libero officiis temporibus accusantium,",
-  ];
-
   // Start mapping, The main array is the (serviceImages), else will be mapped using (index).
   const allServices = serviceImages.map((service, index) => (
     <SingleService
