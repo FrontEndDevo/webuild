1341262b90f56ae03c1a3c4e56f5c685f1ffa464
FrontEndDevo <mylinkacco@gmail.com>
2022-10-28 23:10:59 +0200

    install (free-brands-icons) from Fontawesome

--- src/components/Workers/Workers.jsx
+++ src/components/Workers/Workers.jsx
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 const Workers = () => {
-  return <div>Workers</div>;
+  return (
+    <div>
+      Workers
+      <FontAwesomeIcon icon={faFacebook} />
+    </div>
+  );
 };
 
 export default Workers;
