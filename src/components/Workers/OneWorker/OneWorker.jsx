66e805b47a2a615db86aff10ae93fcd0582045a0
FrontEndDevo <mylinkacco@gmail.com>
2022-10-29 00:13:22 +0200

    Almost finish OneWorker component

--- src/components/Workers/OneWorker/OneWorker.jsx
+++ src/components/Workers/OneWorker/OneWorker.jsx
@@ -3,18 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const OneWorker = (props) => {
   // Split social icons here:
   const socialMedia = props.socialMedia.map((social) => (
-    <FontAwesomeIcon icon={social.icon} />
+    <FontAwesomeIcon icon={social} />
   ));
 
   return (
-    <div className="worker">
-      <div className="person">
-        <img src={props.eachWorker.img} alt={props.eachWorker.name} />
-        <div className="social">{socialMedia}</div>
+    <div className="worker bg-white">
+      <div className="person flex">
+        <img
+          src={props.eachWorker.img}
+          alt={props.eachWorker.name}
+          className="w-5/6"
+        />
+        <div className="social flex flex-col justify-between p-4 text-gray-500 w-1/6">
+          {socialMedia}
+        </div>
       </div>
-      <div className="details">
-        <h3>{props.eachWorker.name}</h3>
-        <p>{props.eachWorker.job}</p>
+      <div className="details p-4">
+        <h3 className="uppercase font-bold text-2xl">
+          {props.eachWorker.name}
+        </h3>
+        <p className="text-gray-500">{props.eachWorker.job}</p>
       </div>
     </div>
   );
--- src/components/Workers/Workers.jsx
+++ src/components/Workers/Workers.jsx
@@ -7,7 +7,7 @@ const Workers = () => {
     <OneWorker socialMedia={workersIcons} eachWorker={worker} />
   ));
   return (
-    <div className="bg-gray-500">
+    <div className="bg-gray-500 py-12 my-12">
       <h2 className="text-center mb-14 text-5xl uppercase font-bold leading-tight">
         We are
         <span className="text-orange-500">
