0c2ea4513937f1ee00ff7c823811ee7c4f24b804
FrontEndDevo <mylinkacco@gmail.com>
2022-10-28 23:20:09 +0200

    Initiate the basic form of workers

--- src/components/Workers/Workers.jsx
+++ src/components/Workers/Workers.jsx
@@ -3,8 +3,26 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 const Workers = () => {
   return (
     <div>
-      Workers
-      <FontAwesomeIcon icon={faFacebook} />
+      <h2 className="text-center mb-14 text-5xl uppercase font-bold leading-tight">
+        We are
+        <span className="text-orange-500">
+          {" "}
+          professional &<br /> expert{" "}
+        </span>
+        workers
+      </h2>
+      <div className="workers">
+        <div className="worker">
+          <div className="person">
+            <img src="" alt="" />
+            <div className="social"></div>
+          </div>
+          <div className="details">
+            <h3>Adam Philips</h3>
+            <p>CEO & Founder</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
