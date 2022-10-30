5e4e61abc842d66420adad7f27c6c76a523ce540
FrontEndDevo <mylinkacco@gmail.com>
2022-10-30 17:35:53 +0200

    Mapping on articles which in (WebsiteData) in (Articles) component

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,17 +1,24 @@
-import { mainArticles } from "../../Helpers/WebsiteData";
+import { mainArticles, articlesIcons } from "../../Helpers/WebsiteData";
+import AnArticle from "./AnArticle/AnArticle";
 
 const Articles = () => {
-
-  // const allArticles = mainArticles.map()
+  const allArticles = mainArticles.map((article) => (
+    <AnArticle
+      img={article.img}
+      title={article.title}
+      author={article.author}
+      icons={articlesIcons}
+    />
+  ));
 
   return (
-    <div className="articles bg-gray-600">
+    <div className="articles">
       <h2 className="text-center mb-14 text-5xl uppercase font-bold leading-tight">
         Latest
         <span className="text-orange-600"> articles </span>
         from <br /> our blog post
       </h2>
-      <div className="main-articles"></div>
+      <div className="main-articles flex gap-12">{allArticles}</div>
     </div>
   );
 };
