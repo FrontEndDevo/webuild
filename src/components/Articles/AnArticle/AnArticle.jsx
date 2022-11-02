b118129c59e1496194df0aa84414d4d9351ec160
FrontEndDevo <mylinkacco@gmail.com>
2022-11-02 12:40:07 +0200

    Finish AnArticle component styles

--- src/components/Articles/AnArticle/AnArticle.jsx
+++ src/components/Articles/AnArticle/AnArticle.jsx
@@ -2,20 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AnArticle = (props) => {
   return (
-    <div className="signle-article bg-gray-400">
+    <div className="signle-article bg-gray-100">
       <img src={props.img} alt={props.title} />
-      <div className="author">
-        <div className="info">
-          <img src={props.author.photo} alt={props.author.name} />
-          <p>{props.author.name}</p>
+      <div className="author p-6 flex justify-between">
+        <div className="info flex items-center gap-2">
+          <img
+            src={props.author.photo}
+            alt={props.author.name}
+            className="rounded-full w-12"
+          />
+          <p className="text-gray-400">{props.author.name}</p>
         </div>
-        <div className="published-date">
-          <FontAwesomeIcon icon={props.icons[0]} />
+        <div className="published-date text-gray-400 flex gap-2 items-center">
+          <FontAwesomeIcon icon={props.icons[0]} className="text-orange-600" />
           <p>{props.author.date}</p>
         </div>
       </div>
-      <h2>{props.title}</h2>
-      <button className="relative uppercase text-orange-500 before:absolute before:-bottom-1 before:left-1/2 before:w-0 before:h-1 before:bg-orange-500 before:duration-500 before:ease-out before:hover:left-0 before:hover:w-full">
+      <h2 className="px-6 font-bold text-2xl">{props.title}</h2>
+      <button className="px-6 py-4 relative uppercase text-orange-500 hover:text-orange-700">
         Read More <FontAwesomeIcon icon={props.icons[1]} />
       </button>
     </div>
