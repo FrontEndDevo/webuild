d960afb72ca2f90121bd8c8964b0a6a2786ebff3
FrontEndDevo <mylinkacco@gmail.com>
2022-11-09 12:45:31 +0200

    Styling BigProject component

--- src/components/Projects/BigProject/BigProject.jsx
+++ src/components/Projects/BigProject/BigProject.jsx
@@ -6,13 +6,13 @@ const BigProject = (props) => {
   const { img, title, address } = props;
 
   return (
-    <div className="project">
-      <img src={img} alt={title} />
-      <Link to="/" className="details">
-        <h3>{title}</h3>
+    <div className="project relative">
+      <img src={img} alt={title} className="h-full" />
+      <Link to="/" className="details bg-white p-6 absolute -bottom-12 left-6 w-11/12">
+        <h3 className="text-2xl uppercase font-bold mb-2">{title}</h3>
         <div className="address">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <span>{address}</span>
+          <FontAwesomeIcon icon={faLocationDot} className="text-orange-600 mr-2" />
+          <span className="text-gray-500">{address}</span>
         </div>
       </Link>
     </div>
