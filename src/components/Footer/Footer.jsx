2a5946d0e14d54170344fbd8e086342a5711ef2f
FrontEndDevo <mylinkacco@gmail.com>
2022-11-10 14:37:59 +0200

    Finish Footer componen structure (JSX code)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,83 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBuilding,
-  faLocationDot,
-  faPhone,
-  faEnvelope,
-} from "@fortawesome/free-solid-svg-icons";
+import { footerIcons } from "../../Helpers/WebsiteData";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="R-H-S">
-        <div className="flex text-6xl gap-2">
-          <FontAwesomeIcon icon={faBuilding} className="text-orange-700" />
-          <h1 className="font-bold">webuild</h1>
+      <div className="top-side">
+        <div className="R-H-S">
+          <div className="flex text-6xl gap-2">
+            <FontAwesomeIcon
+              icon={footerIcons.faBuilding}
+              className="text-orange-700"
+            />
+            <h1 className="font-bold">webuild</h1>
+          </div>
+          <p>
+            Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem
+            nonumy. Tempor sea ipsum diam sed clita dolore eos dolores magna
+            erat dolore sed stet justo et dolor.
+          </p>
+          <div className="contact">
+            <div className="info">
+              <FontAwesomeIcon icon={footerIcons.faLocationDot} />
+              <p>123 Street, New York, USA</p>
+            </div>
+            <div className="info">
+              <FontAwesomeIcon icon={footerIcons.faPhone} />
+              <p>+012 345 67890</p>
+            </div>
+            <div className="info">
+              <FontAwesomeIcon icon={footerIcons.faEnvelope} />
+              <p>info@example.com</p>
+            </div>
+          </div>
+          <div className="social">
+            <FontAwesomeIcon icon={footerIcons.faTwitter} />
+            <FontAwesomeIcon icon={footerIcons.faFacebook} />
+            <FontAwesomeIcon icon={footerIcons.faLinkedin} />
+            <FontAwesomeIcon icon={footerIcons.faInstagram} />
+          </div>
         </div>
+        <div className="L-H-S">
+          <div className="up-level">
+            <div className="left-side">
+              <h3>Quick links</h3>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Our Service</li>
+                <li>Meet The Team</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+            <div className="right-side">
+              <h3>Popular links</h3>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Our Service</li>
+                <li>Meet The Team</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="down-level">
+            <form>
+              <label htmlFor="email">Newsletter</label>
+              <input type="email" id="email" />
+              <button>Sign up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-side">
         <p>
-          Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy.
-          Tempor sea ipsum diam sed clita dolore eos dolores magna erat dolore
-          sed stet justo et dolor.
+          &copy; <span> We Build.</span>All Rights Reserved.
         </p>
-        <div className="contact">
-          <div className="info">
-            <FontAwesomeIcon icon={faLocationDot} />
-            <p>123 Street, New York, USA</p>
-          </div>
-          <div className="info">
-            <FontAwesomeIcon icon={faPhone} />
-            <p>+012 345 67890</p>
-          </div>
-          <div className="info">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <p>info@example.com</p>
-          </div>
-        </div>
+        <p>
+          <span>Designed by</span> FrontEndDevo
+        </p>
       </div>
     </div>
   );
