92b8d0cdc9aba2dff6dab593e132b0d07c725d24
FrontEndDevo <mylinkacco@gmail.com>
2022-11-10 14:24:40 +0200

    Before seperate icons to WebsiteData function

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faLocationDot,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="R-H-S">
+        <div className="flex text-6xl gap-2">
+          <FontAwesomeIcon icon={faBuilding} className="text-orange-700" />
+          <h1 className="font-bold">webuild</h1>
+        </div>
+        <p>
+          Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy.
+          Tempor sea ipsum diam sed clita dolore eos dolores magna erat dolore
+          sed stet justo et dolor.
+        </p>
+        <div className="contact">
+          <div className="info">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <p>123 Street, New York, USA</p>
+          </div>
+          <div className="info">
+            <FontAwesomeIcon icon={faPhone} />
+            <p>+012 345 67890</p>
+          </div>
+          <div className="info">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <p>info@example.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
