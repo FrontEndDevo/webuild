29d4c61e2079300a39e74d49cf4ded111e782e7b
FrontEndDevo <mylinkacco@gmail.com>
2022-11-14 23:16:02 +0200

    Finish Client structure

--- src/components/Cleints/Cleints.jsx
+++ src/components/Cleints/Cleints.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Cleints = () => {
-  return <div>Cleints</div>;
+  return (
+    <div className="clients text-center bg-gray-200 py-24">
+      <h3 className="uppercase text-5xl font-bold">
+        What our{" "}
+        <span className="text-orange-600 ">
+          {" "}
+          happy <br /> clients{" "}
+        </span>
+        say!!!
+      </h3>
+      <div className="content">
+        <img src="" alt="" />
+        <div className="client">
+          <img src="" alt="" />
+          <div className="client-info">
+            <h3>Client name</h3>
+            <p>profession</p>
+            <span>
+              <FontAwesomeIcon icon={faQuoteLeft} /> Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Doloribus, ipsam.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cleints;
